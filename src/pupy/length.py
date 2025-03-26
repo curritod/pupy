d338d81b71208400f9bd5f49c4396eb9a5d5973b
Diogo Ribeiro <diogo.gnc.ribeiro@gmail.com>
2025-03-26 14:17:38 +0100

    length units

---
+++ src/pupy/length.py
@@ -0,0 +1,12 @@
+from .prefixes import *
+
+meter = 1
+
+picometer   = pico*meter
+nanometer   = nano*meter
+micrometer  = micro*meter
+milimeter   = mili*meter
+centimeter  = centi*meter
+kilometer   = kilo*meter
+
+Angstrom    = 1e-1*nanometer
