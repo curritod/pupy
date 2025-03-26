aeda063f9acb3ef41917966b5d6291420b9a7e40
Diogo Ribeiro <diogo.gnc.ribeiro@gmail.com>
2025-03-26 13:54:45 +0100

    prefixes added

---
+++ src/pupy/prefixes.py
@@ -0,0 +1,21 @@
+deci  = 1e-1
+centi = 1e-2
+mili  = 1e-3
+micro = 1e-6
+nano  = 1e-9
+pico  = 1e-12
+femto = 1e-15
+atto  = 1e-18
+zepto = 1e-21
+yocto = 1e-24
+
+deka  = 1e1
+hecto = 1e2
+kilo  = 1e3
+mega  = 1e6
+giga  = 1e9
+tera  = 1e12
+peta  = 1e15
+exa   = 1e18
+zetta = 1e21
+yotta = 1e24
