b54664095919622974dd98c1d6db2fe7a7ae5622
Diogo Ribeiro <diogo.gnc.ribeiro@gmail.com>
2025-03-26 13:55:38 +0100

    angular conversion factors

---
+++ src/pupy/angles.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+deg_to_rad = np.pi/180
+rad_to_deg = 1/deg_to_rad
+
+rad_to_as = (180/np.pi)*60*60
+as_to_rad = 1/rad_to_as
+
+rad_to_mas = (180/np.pi)*60*60*1e3
+rad_to_microas = (180/np.pi)*60*60*1e6
+
+mas_to_rad = 1/rad_to_mas
+microas_to_rad = 1/rad_to_microas
+
+
